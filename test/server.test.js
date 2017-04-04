5b632f7d9451d4e4da5ffe460a78593af941ec56
Darren M <aeneascorrupt@gmail.com>
2017-04-03 22:35:13 -0700

    artist endpoints

--- test/server.test.js
+++ test/server.test.js
@@ -1,6 +1,10 @@
 'use strict';
 
+const env = require('node-env-file');
 const expect = require('chai').expect;
+const path = require('path');
+
+env(path.resolve(__dirname, '../.env'), { raise: false });
 
 describe('server test', () => {
   let server;
@@ -29,6 +33,41 @@ describe('server test', () => {
       expect(payload).to.deep.equal({
         status: 'OK'
       });
+    });
+  });
+
+  it('returns the top tracks of an artist', () => {
+    return server.inject({
+      method: 'GET',
+      url: '/api/artists/rick%20astley/tracks'
+    }).then((response) => {
+      expect(response.statusCode).to.equal(200);
+
+      const payload = JSON.parse(response.payload);
+
+      expect(payload.data).to.be.an('array')
+        .of.length(50);
+
+      const targetTrack = payload.data[0];
+
+      expect(targetTrack.name).to.equal('Never Gonna Give You Up');
+      expect(targetTrack.playCount).to.be.above(2165960);
+      expect(targetTrack.listeners).to.be.above(435225);
+    });
+  });
+
+  it('returns the artist info', () => {
+    return server.inject({
+      method: 'GET',
+      url: '/api/artists/rick%20astley'
+    }).then((response) => {
+      expect(response.statusCode).to.equal(200);
+
+      const payload = JSON.parse(response.payload);
+
+      expect(payload.data).to.be.an('object');
+
+      expect(payload.data.name).to.equal('Rick Astley');
     });
   });
 });
