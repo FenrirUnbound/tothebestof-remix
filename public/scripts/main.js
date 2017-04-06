416a72710105a88c7a54434221e41f4152079c31
Darren M <aeneascorrupt@gmail.com>
2017-04-05 23:08:23 -0700

    embed video of top tracks

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const MAX_TRACKS = 10;
+
 const app = angular.module('ttbo', [
   'ngSanitize'
 ]);
@@ -15,10 +17,38 @@ function getTopTracks($http, artist) {
   return $http({
     method: 'GET',
     url: `/api/artists/${artist}/tracks`
-  }).then(response => Promise.resolve(response.data.data));
+  }).then(response => Promise.resolve(response.data.data.slice(0, MAX_TRACKS)));
 }
 
-app.controller('AppController', ['$scope', '$http', '$compile', function ($scope, $http, $compile) {
+function createVid($scope, videoIds) {
+  const first = videoIds.shift();
+
+  const player = new YT.Player('videoPort', {
+    height: '270',
+    width: '480',
+    playerVars: {
+      playlist: videoIds.join(',')
+    },
+    videoId: first
+  });
+}
+
+function getVideos($http, artist, tracks) {
+  const request = ($http, artist, track) => {
+    return $http({
+      method: 'GET',
+      url: encodeURI(`/api/artists/${artist}/videos/${track}`)
+    });
+  };
+
+  const videoRequests = tracks.map((track) => request($http, artist, track.name));
+
+  return Promise.all(videoRequests).then(results =>
+    results.map((video) => video.data.data.id.videoId)
+  );
+}
+
+app.controller('AppController', ['$scope', '$http', function ($scope, $http) {
   $scope.searchText = '';
   $scope.artist = {
     name: '',
@@ -26,6 +56,9 @@ app.controller('AppController', ['$scope', '$http', '$compile', function ($scope
     image: ''
   };
   $scope.topTracks = [];
+  $scope.video = {
+    link: ''
+  };
 
   $scope.search = () => {
     const artistName = $scope.searchText;
@@ -35,6 +68,10 @@ app.controller('AppController', ['$scope', '$http', '$compile', function ($scope
     const fetchArtistInfo = getArtist($http, artistName);
     const fetchTopTracks = getTopTracks($http, artistName);
 
+    const renderVideo = fetchTopTracks
+      .then(topTracks => getVideos($http, artistName, topTracks))
+      .then(videoIds => createVid($scope, videoIds));
+
     return Promise.all([
       fetchArtistInfo,
       fetchTopTracks
@@ -42,7 +79,7 @@ app.controller('AppController', ['$scope', '$http', '$compile', function ($scope
       $scope.artist = artistInfo;
 
       const maxPopularity = topTracks[0].listeners;
-      $scope.topTracks = topTracks.slice(0, 10).map((track) => {
+      $scope.topTracks = topTracks.map((track) => {
         return {
           name: track.name,
           popularity: track.listeners / maxPopularity * 100
@@ -50,6 +87,8 @@ app.controller('AppController', ['$scope', '$http', '$compile', function ($scope
       });
 
       $scope.$apply();
+
+      return renderVideo;
     });
   };
 }]);
